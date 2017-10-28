02d05e2b50cd64178f26dd9cbd3a9daa6ae1578d
Cmell <clmellinger@gmail.com>
2017-10-28 15:52:15 -0600

    Working estimation function.

---
+++ R/DiffusionEstimation.R
@@ -0,0 +1,74 @@
+
+diffEst <- function (
+  pid,
+  dataDir, 
+  logDir,
+  baseCtl,
+  returnResult = F,
+  writeResult = T,
+  df=NULL,
+  varNames=NULL,
+  fastDmLoc='./fast-dm'
+) {
+  #' Estimates a single diffusion model via fast-dm.
+  #'
+  #' @param pid The file prefix on the ".dat" file to process.
+  #' @param dataDir The directory containing data files to process.
+  #' @param logDir The directory to save log files to.
+  #' @param baseCtl A character string containing the basic control file without
+  #' a load or log statement.
+  #' @param returnResult If TRUE (not default), a dataframe with 1 row is 
+  #' returned containing the results of the estimation.
+  #' @param writeResult If TRUE (default) the result is written to a .lst file
+  #' in the `logDir`.
+  #' @param df Dataframe to process data from. Currently not tested.
+  #' @param varNames Character vector of variable names to use from `df`.
+  #' @param fastDmLoc Path to the fast-dm executable, including the names of the
+  #' executable.
+  #'
+  #' @return If 
+  #' @export
+  
+  # Note that baseCtl should not have a load or log statement. This is added.
+  datFl <- paste0(dataDir, '/', pid, '.dat')
+  logFl <- paste0(logDir, '/', pid, '.lst')
+  ctlFl <- paste0(dataDir, '/', pid, '.ctl')
+  
+  if (!dir.exists(logDir)) {
+    dir.create(logDir)
+  }
+  
+  # Write the data file if no df if provided
+  rmDatFl <- F
+  if (!is.null(df)) {
+    # doesn't work yet
+    if (is.null(varNames)) {
+      varNames <- colnames(df)
+    }
+    write.table(df, file=datFl, sep=" ", col.names = F, row.names = F)
+    rmDatFl <- T
+  }
+  
+  # Write the control file
+  ctl <- paste0(baseCtl, '\nload ', datFl, '\n', 'log ', logFl)
+  fileConn <- file(ctlFl)
+  writeLines(ctl, fileConn)
+  close(fileConn)
+  
+  # Construct the command
+  sysCmd <- paste(fastDmLoc, ctlFl)
+  system(sysCmd)
+  
+  # Clean up
+  file.remove(ctlFl)
+  
+  # Return the result
+  if (returnResult) {
+    # Read the output
+    res <- read.table(logFl, header=T)
+    if (!writeResult) {
+      file.remove(logFl)
+    }
+    return(res)
+  }
+}
